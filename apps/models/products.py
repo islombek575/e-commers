eec139cfdb9ee71d1841595b5ba323c11b5ea999
islombek575 <hoshimovislombek023@gmail.com>
2025-10-02 11:01:56 +0500

    add commit

--- apps/models/products.py
+++ apps/models/products.py
@@ -1,19 +1,23 @@
 
-from django.db.models import Model, ForeignKey, CASCADE, ImageField
-from django.db.models.fields import CharField, DecimalField, BooleanField
+from django.db.models import Model, ForeignKey, CASCADE, ImageField, TextChoices
+from django.db.models.fields import CharField, DecimalField, BooleanField, IntegerField
 
 
 class Category(Model):
+    icon = ImageField(upload_to='media/icon/')
     name = CharField(unique=True)
 
 
 class Product(Model):
+    category = ForeignKey('apps.Category', on_delete=CASCADE)
     name = CharField(max_length=100)
     price = DecimalField(decimal_places=2, max_digits=10)
     discount_price = DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
     color = CharField(max_length=100, null=True, blank=True)
-    in_stock = BooleanField(default=True)
+    stock = IntegerField()
     description = CharField(null=True, blank=True)
+    size = CharField(max_length=100)
+
 
 
 class ProductImage(Model):
---
+++ apps/models/users.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Model
+from django.db.models.fields import CharField
+
+
+class User(Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100, null=True, blank=True)
+    address = CharField(max_length=100, null=True, blank=True)
+    email = CharField(max_length=100, unique=True)
+    phone = CharField(max_length=100, unique=True)
+    password = CharField(max_length=100)
+
+
+
+class AdminUser(AbstractUser):
+    pass
